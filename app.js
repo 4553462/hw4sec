d939bcd6e83d457e9b4836a20a76a3ab5df37dc9
Indira_Matieva <122766793+4553462@users.noreply.github.com>
2023-03-06 01:06:43 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const re = document.getElementById('re')
+const getData = async() => {
+    const te = await fetch ('https://jsonplaceholder.typicode.com/posts')
+    const fr = await te.json()
+
+    const changedUsers = fr.map ((el) => {
+        return `<div class = "di"> <p> userId: </p> ${el.userId} <p> id: </p> ${el.id} ${el.title} ${el.body} </div>`
+   })
+   re.innerHTML = changedUsers.join("");
+
+}
+getData()
